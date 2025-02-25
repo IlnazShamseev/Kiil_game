6f18e8225b25c15ee4b9111fd6b52830cca3eff2
World <161411405+IlnazShamseev@users.noreply.github.com>
2025-02-25 12:13:16 +0300

    Add files via upload

---
+++ data/maps/cb9c43bc61860f362ef667f07cd2bc67.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="cb9c43bc61860f362ef667f07cd2bc67" tilewidth="32" tileheight="32" tilecount="900" columns="30">
+ <image source="../../../../Downloads/cb9c43bc61860f362ef667f07cd2bc67.png" trans="000000" width="960" height="960"/>
+</tileset>
